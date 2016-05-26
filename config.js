55975db56eea9f0c32c9aed768e5ab015a7891e4
Wildan <garviandiwildan@gmail.com>
2016-05-26 23:51:01 +0700

    config prod env DB

--- config.js
+++ config.js
@@ -10,7 +10,12 @@ config.env = process.env.NODE_ENV || 'development';
 config.port = process.env.PORT || 2001;
 if (config.env == 'production') {
     config.postgresDB = {
-
+        host:'ec2-54-243-202-113.compute-1.amazonaws.com',
+        database:'d4ctoo3avpc4hj',
+        user:'yspwyfxyheiwzu',
+        port:5432,
+        password:'5KUKARRj4iKEtgJzyf-Hx88KYr',
+        
     }
 } else {
     config.postgresDB = {
